a7061e13bd54e20c6d777f36b98e410e7a48698c
Charlie B <>
2020-10-16 12:01:45 +0100

    update model name

--- mysite/main/models.py
+++ mysite/main/models.py
@@ -2,15 +2,15 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
-class ToDoList(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="todolist", null=True)
+class Budget(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="budget", null=True)
     name = models.CharField(max_length = 200)
     
     def __str__(self):
         return self.name
     
 class Item(models.Model):
-    todolist = models.ForeignKey(ToDoList, on_delete = models.CASCADE)
+    budget = models.ForeignKey(Budget, on_delete = models.CASCADE)
     text = models.CharField(max_length = 300)
     cost = models.CharField(max_length = 300, null=True)
     
