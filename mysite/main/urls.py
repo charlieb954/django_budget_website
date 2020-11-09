6ad5463600cc063453c69a0703d32764d07a6503
Charlie B <>
2020-11-09 10:33:33 +0000

    add summary url

--- mysite/main/urls.py
+++ mysite/main/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path("home/", views.home, name = "home"),
     path("create/", views.create , name = "create"),
     path("view/", views.view , name = "view"),
+    path("summary/", views.budget_summary, name="summary")
 ]
